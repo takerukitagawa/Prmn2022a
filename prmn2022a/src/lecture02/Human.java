b702acecd915e281e14070fa14cfd610c322044c
takerukitagawa <cist.b221.takeru.kitagawa@gmail.com>
2022-11-03 16:50:58 +0900

    Exercise2_1

---
+++ prmn2022a/src/lecture02/Human.java
@@ -0,0 +1,20 @@
+package lecture02;
+
+public class Human {
+    String name;
+    int age;
+
+    Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void print(){
+        if(age <= 18){
+            System.out.println("生徒:" + name + " " + age + "歳です。");
+        }else{
+            System.out.println("学生:" + name + " " + age + "歳です。");
+        }
+    }
+
+}
