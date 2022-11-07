da483157e74431c56e6c84f463a0a59732d3e203
takerukitagawa <cist.b221.takeru.kitagawa@gmail.com>
2022-11-07 16:22:18 +0900

    Exercise2_2

--- prmn2022a/src/lecture02/Exercise2_3.java
+++ prmn2022a/src/lecture02/Exercise2_3.java
@@ -3,6 +3,9 @@ package lecture02;
 public class Exercise2_3 {
     public static void main(String[] args) {
         Tire[] tire = new Tire[4];
+        for(int i = 0; i< tire.length; i++){
+            tire[i] = new Tire(65);
+        }
         Engine engine = new Engine(4000);
         Car car = new Car(tire, engine);
         GasStation gasStation = new GasStation();
